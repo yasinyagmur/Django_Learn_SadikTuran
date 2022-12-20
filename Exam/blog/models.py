ab748d81e632b7afef75591401d897304a3a0998
yasin yağmur <yasin.yagmur@gmail.com>
2022-12-20 23:03:04 +0300

    fixed

--- Exam/blog/models.py
+++ Exam/blog/models.py
@@ -1,11 +1,28 @@
 from distutils.command.upload import upload
 from pyexpat import model
+from unicodedata import category
 from django.db import models
 from django.utils.text import slugify
 from ckeditor.fields import RichTextField
 
 
 # Create your models here.
+
+# localhost/category/beyaz-esya
+class Category(models.Model):
+    name=models.CharField(max_length=150)
+    slug=models.SlugField(null=False,blank=True, unique=True,db_index=True,editable=False)
+    
+    def save(self,*args,**kwargs):
+        self.slug=slugify(self.name)
+        super().save(*args,**kwargs)
+
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+
 
 #blogs/1.jppeg 
 #movies/5.jppeg Image upload adresleme 
@@ -20,6 +37,7 @@ class Blog(models.Model):
     #  model de bulunan datalara slug eklendikten sonra silinir ya da false yapılır default değeri false
     # slug=models.SlugField(null=True,blank=True,unique=True,db_index=True)
     slug=models.SlugField(null=False,blank=True,unique=True,db_index=True,editable=False)
+    category=models.ForeignKey(Category,null=True,on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.title}"
@@ -28,16 +46,3 @@ class Blog(models.Model):
     def save(self,*args,**kwargs):
         self.slug=slugify(self.title)
         super().save(*args,**kwargs)
-
-# localhost/category/beyaz-esya
-class Category(models.Model):
-    name=models.CharField(max_length=150)
-    slug=models.SlugField(null=False,blank=True, unique=True,db_index=True,editable=False)
-    
-    def save(self,*args,**kwargs):
-        self.slug=slugify(self.name)
-        super().save(*args,**kwargs)
-
-
-    def __str__(self):
-        return f"{self.name}"
